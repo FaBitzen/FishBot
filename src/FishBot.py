5dfefa37bfb2efa69df24cf069d4a153e5b76c91
Fabian <fabitzen@gmail.com>
2021-02-02 14:33:11 +0100

    finished basic DadBot functionalities.

---
+++ src/FishBot.py
@@ -0,0 +1,29 @@
+import discord
+import regex
+import keys.privatekey as ps
+import random
+
+
+class FishBot(discord.Client):
+    # einloggen
+    async def on_ready(self):
+        print("ready")
+
+    # bei Nachricht
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        pattern = "[Ii]['`´]?[Mm] ([^,\.\n\r]+)"
+        match = regex.match(pattern, message.content)
+
+        greetings = ["Hi, ", "Hey, ", "Howdy, "]
+
+        if match:
+            await message.channel.send(random.choice(greetings) + match[1] +
+                                       ", I'm dad.")
+
+
+if __name__ == "__main__":
+    client = FishBot()
+    client.run(ps.privatekey())
